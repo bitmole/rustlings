330fb3f8497ba848cb1d0538753f4d80d2179b68
bitmole <robert@idea-loop.com>
2023-08-01 16:19:01 -0600

    More exercises.

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -34,7 +34,7 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    map.values().filter(|x| *x == &value).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -53,7 +53,10 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    let to_count = |m: &HashMap<String, Progress>| -> usize { 
+        m.values().filter(|x| *x == &value).count() 
+    };
+    collection.iter().map(|x| to_count(x)).sum()
 }
 
 #[cfg(test)]
